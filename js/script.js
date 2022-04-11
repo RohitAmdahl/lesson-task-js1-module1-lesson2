7e089733d8492ee6008610af09703073d614ba80
Rohit kumar Amdahl <rohit_designer@outlook.com>
2022-04-12 00:41:21 +0200

    Update script.js

---
+++ js/script.js
@@ -0,0 +1,12 @@
+const heading = document.querySelector("h3")
+
+heading .innerHTML = "Hello,Welcome";
+
+const margin= document.querySelector("h4")
+
+margin .style.color="blue"
+margin .style.padding="20px"
+margin .style.margin="40px"
+margin .style.fontSize="40px"
+margin .style.backgroundColor="red"
+
