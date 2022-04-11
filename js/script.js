c9b65f9c7e0716e0f40edda6453b6cd5a18ecf46
Rohit kumar Amdahl <rohit_designer@outlook.com>
2022-04-12 01:03:29 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -2,11 +2,21 @@ const heading = document.querySelector("h3")
 
 heading .innerHTML = "Hello,Welcome";
 
-const margin= document.querySelector("h4")
+const margin= document.querySelector(".two")
 
 margin .style.color="blue"
 margin .style.padding="20px"
 margin .style.margin="40px"
 margin .style.fontSize="40px"
 margin .style.backgroundColor="red"
+
+
+
+const div = document.querySelector(".content")
+
+console.log(div.innerHTML);
+
+div.innerHTML= "<p>buy</p>"
+
+
 
